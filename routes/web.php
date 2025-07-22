9a8d142d6f36c5cd1a0d76d48bbf58afda3ad7e7
Neil Mallia <neil.mallia@9hdigital.com>
2025-07-22 11:30:07 +0200

    Update: adjusted grouped routes based on the nova path to fix the login redirect bug

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuditTrailController;
 use Illuminate\Support\Facades\Route;
 
 //Audit Trails Routes
-Route::group(['prefix' => 'audit-trails'], function() {
+Route::group(['prefix' => 'admin/audit-trails'], function() {
     Route::get('/login', [AuditTrailController::class, "login"])->name('audit-trails.login');
     Route::get('/system', [AuditTrailController::class, "system"])->name('audit-trails.system');
 });
