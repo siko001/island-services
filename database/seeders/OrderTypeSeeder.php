8d47f1fdecb14ff2f90a6a0dfa9d0c9205bf1bdc
Neil Mallia <neil.mallia@9hdigital.com>
2025-07-22 12:26:23 +0200

    Update: created the DB seeder for the OrderType with different defaults

---
+++ database/seeders/OrderTypeSeeder.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\OrderType;
+use Illuminate\Database\Seeder;
+
+class OrderTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+    public function run(): void
+    {
+        $orderTypes = [
+            [
+                'name' => 'Direct Sales',
+                'abbreviation' => 'Direct',
+                'is_default' => false,
+                'short_period_type' => rand(0, 1),
+            ],
+
+            [
+                'name' => 'Answering Machine',
+                'abbreviation' => 'AM',
+                'is_default' => true,
+                'short_period_type' => rand(0, 1),
+            ],
+            [
+                'name' => 'Standing Order',
+                'abbreviation' => 'SO',
+                'is_default' => false,
+                'short_period_type' => rand(0, 1),
+            ],
+            [
+                'name' => 'Trail Period',
+                'abbreviation' => 'Trial',
+                'is_default' => false,
+                'short_period_type' => rand(0, 1),
+            ],
+            [
+                'name' => 'Fax',
+                'abbreviation' => 'Fax',
+                'is_default' => false,
+                'short_period_type' => rand(0, 1),
+            ],
+            [
+                'name' => 'Phone Call',
+                'abbreviation' => 'phone',
+                'is_default' => false,
+                'short_period_type' => rand(0, 1),
+            ],
+            [
+                'name' => 'Website',
+                'abbreviation' => 'website',
+                'is_default' => false,
+                'short_period_type' => rand(0, 1),
+            ],
+            [
+                'name' => 'Salesman',
+                'abbreviation' => 'sales',
+                'is_default' => false,
+                'short_period_type' => rand(0, 1),
+            ],
+            [
+                'name' => 'Driver',
+                'abbreviation' => 'driver',
+                'is_default' => false,
+                'short_period_type' => rand(0, 1),
+            ],
+            [
+                'name' => 'Trail Period',
+                'abbreviation' => 'Trial',
+                'is_default' => false,
+                'short_period_type' => rand(0, 1),
+            ],
+        ];
+
+        foreach($orderTypes as $orderType) {
+            OrderType::updateOrCreate(
+                ['name' => $orderType['name']],
+                $orderType
+            );
+        }
+    }
+}
