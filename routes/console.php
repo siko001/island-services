a79e6d32411a066613529ddf5d9b9d04abc3569a
Neil Mallia <neil.mallia@9hdigital.com>
2025-07-31 12:15:24 +0200

    Updated the Artisan command to create tenancy to ask for logo path and give default, refactor the returns

--- routes/console.php
+++ routes/console.php
@@ -16,28 +16,37 @@ Artisan::command('permissions:generate:crud', function() {
 
 //Create the tenancy Via Artisan command
 Artisan::command('custom-tenant:create', function(Request $request) {
+
     $tenantName = $this->ask('Enter the tenant name');
     $tenantDomain = strtolower(str_replace(' ', '-', $tenantName));
-
-    $superAdminEmail = $this->ask('Enter the super admin email');
-    $superAdminPassword = $this->ask('Enter the super admin password');
-
     if(empty($tenantName)) {
         $this->error('Tenant name cannot be empty.');
         return;
     }
 
+    $superAdminEmail = $this->ask('Enter the super admin email');
     if(empty($superAdminEmail)) {
         $this->error('Super Admin Email cannot be empty.');
         return;
     }
+
+    $superAdminPassword = $this->ask('Enter the super admin password');
     if(empty($superAdminPassword)) {
         $this->error('Super Admin Password cannot be empty.');
         return;
     }
 
+    $tenantLogo = $this->ask('Enter the tenant logo_path Ex. /media/images/H2Only.png (optional, press enter to skip. Default is the isl-logo.svg)');
+
+    // Check if the tenant already exists
+    if(\App\Models\Tenant::where('id', $tenantName)->exists()) {
+        $this->error('Tenant with this name already exists.');
+        return;
+    }
+
     $tenant = \App\Models\Tenant::create(['id' => $tenantName]);
     $tenant->domains()->create(['domain' => $tenantDomain . '.' . config('tenancy.central_domains')[0]]);
+    $tenant->logo_path = $tenantLogo ?: '/media/images/isl-logo.svg';
 
     $tenant->run(function() use ($tenant, $superAdminEmail, $superAdminPassword) {
         // Run migrations for the tenant
