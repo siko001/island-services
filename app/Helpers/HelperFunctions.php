40627ef94cc5026ef610761a14d4fcaf5cc75f50
Neil Mallia <neil.mallia@9hdigital.com>
2025-08-09 09:49:15 +0200

    seperated permission creation in it's own file

--- app/Helpers/HelperFunctions.php
+++ app/Helpers/HelperFunctions.php
@@ -5,9 +5,6 @@ namespace App\Helpers;
 use App\Models\Admin\Role;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Str;
-use Laravel\Nova\Nova;
-use Spatie\Permission\Models\Permission;
 
 class HelperFunctions
 {
@@ -53,57 +50,10 @@ class HelperFunctions
             ->toArray();
     }
 
-    /**
-     * Create CRUD permissions (view, create, update, delete) for all Nova resources.
-     * @param \Symfony\Component\Console\Output\OutputInterface|null $output
-     * @return int Total number of permissions created
-     */
-    public static function generateCrudPermissionsFromNovaResources($output = null): int
-    {
-        $resources = Nova::$resources;
-        $allActions = ['view', 'view any', 'create', 'update', 'delete'];
-        $permissionModelActions = ['view', 'view any'];
-        $createdCount = 0;
-
-        foreach($resources as $resourceClass) {
-            $model = $resourceClass::$model ?? null;
-            if(!$model)
-                continue;
-
-            $name = class_basename($model);
-            $slug = Str::snake($name);
-
-            // Use limited actions for Permission model only
-            $actions = is_a($model, Permission::class, true)
-                ? $permissionModelActions
-                : $allActions;
-
-            foreach($actions as $action) {
-                $permissionName = "{$action} {$slug}";
-
-                if(!Permission::where('name', $permissionName)->exists()) {
-                    Permission::create(['name' => $permissionName]);
-                    $createdCount++;
-
-                    if($output) {
-                        $output->writeln("Created permission: {$permissionName}");
-                    } else {
-                        echo "Created permission: {$permissionName}\n";
-                    }
-                }
-            }
-        }
-
-        $createdCount = self::createStaticPermissions($output, $createdCount);
-        $createdCount = self::createActionPermissions($output, $createdCount);
-
-        return $createdCount;
-    }
-
     public static function generateCrudPermissions($output = null): void
     {
         try {
-            $count = self::generateCrudPermissionsFromNovaResources($output);
+            $count = PermissionGenerator::generatePermissions($output);;
 
             if($count > 0) {
                 $message = "🌱 CRUD permissions generated successfully. ({$count} new)";
@@ -122,49 +72,6 @@ class HelperFunctions
                 ? $output->writeln("<error>An error occurred while generating CRUD permissions.</error>")
                 : print "An error occurred while generating CRUD permissions.\n";
         }
-    }
-
-    public static function createStaticPermissions($output, $createdCount): int
-    {
-        $staticPermissions = [
-            'view audit_trail_login',
-            'view audit_trail_system',
-            'view other_companies',
-        ];
-        foreach($staticPermissions as $permission) {
-            if(!Permission::where('name', $permission)->exists()) {
-                Permission::create(['name' => $permission]);
-                $createdCount++;
-
-                if($output) {
-                    $output->writeln("Created static permission: {$permission}");
-                } else {
-                    echo "Created static permission: {$permission}\n";
-                }
-            }
-        }
-        return $createdCount;
-    }
-
-    public static function createActionPermissions($output, $createdCount): int
-    {
-        $actionPermissions = [
-            'terminate user',
-        ];
-
-        foreach($actionPermissions as $permission) {
-            $permissionName = "{$permission}";
-            if(!Permission::where('name', $permissionName)->exists()) {
-                Permission::create(['name' => $permissionName]);
-                $createdCount++;
-                if($output) {
-                    $output->writeln("Created action permission: {$permissionName}");
-                } else {
-                    echo "Created action permission: {$permissionName}\n";
-                }
-            }
-        }
-        return $createdCount;
     }
 
     public static function otherDefaultExists($model, $currentId): bool
