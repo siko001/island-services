58e5a2a3d9f504ac8a5841efad968989cae9b38c
Neil Mallia <neil.mallia@9hdigital.com>
2025-08-13 13:03:37 +0200

    cleaned up the group permissions component from all the unnecessary logs

--- nova-components/GroupedPermissions/src/GroupedPermissions.php
+++ nova-components/GroupedPermissions/src/GroupedPermissions.php
@@ -2,7 +2,6 @@
 
 namespace IslandServices\GroupedPermissions;
 
-use Illuminate\Support\Facades\Log;
 use Spatie\Permission\Traits\HasPermissions;
 use Vyuldashev\NovaPermission\PermissionBooleanGroup;
 
@@ -16,23 +15,15 @@ class GroupedPermissions extends PermissionBooleanGroup
 
     protected function fillAttributeFromRequest(\Laravel\Nova\Http\Requests\NovaRequest $request, string $requestAttribute, object $model, string $attribute): void
     {
-        Log::debug('fillAttributeFromRequest called', [
-            'request_attribute' => $requestAttribute,
-            'request_data' => $request->all(),
-        ]);
-
         if(!in_array(HasPermissions::class, class_uses_recursive($model))) {
             throw new \InvalidArgumentException('Model must implement HasPermissions trait.');
         }
 
         if(!$request->exists($requestAttribute)) {
-            Log::debug('Request attribute missing: ' . $requestAttribute);
             return;
         }
 
         $values = $request->input($requestAttribute);
-
-        Log::debug('Permissions sync input values:', ['values' => $values]);
 
         if(is_string($values)) {
             $values = json_decode($values, true) ?: [];
@@ -40,9 +31,6 @@ class GroupedPermissions extends PermissionBooleanGroup
 
         if(is_array($values)) {
             $model->syncPermissions($values);
-            Log::debug('Permissions synced for model ID ' . $model->id);
-        } else {
-            Log::debug('Input values is not an array', ['type' => gettype($values)]);
         }
     }
 }
