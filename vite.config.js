8a400347ae74bbdbe804479ef15e679fac737fb1
Neil Mallia <neil.mallia@9hdigital.com>
2025-09-17 09:39:49 +0200

    added js assests to vite config. added tenancy build directory to gitignore file. removed redundant build assets

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+	plugins: [
+		laravel({
+			input: [
+				'resources/css/app.css',
+				'resources/js/app.js',
+				'resources/js/central/tenancy-delete.js',
+				'resources/js/central/tenancy-edit.js',
+				'resources/js/central/header.js',
+				'resources/js/central/notification.js',
+			],
+			refresh: true,
+		}),
+		tailwindcss(),
+	],
 });
