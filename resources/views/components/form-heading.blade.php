3a81ad913029005dfc4b8621073e4d27cecf9326
Neil Mallia <neil.mallia@9hdigital.com>
2025-09-17 17:30:28 +0200

    More Info when setting up the first time in the central app

--- resources/views/components/form-heading.blade.php
+++ resources/views/components/form-heading.blade.php
@@ -1,7 +1,17 @@
 <div class="mt-16 mb-4">
     @if(!$tenants)
-        <h1 class="font-bold text-2xl sm:text-3xl dark:text-gray-200 text-gray-700">Add Your First Application</h1>
+        @if(!Auth::user())
+             @if(config('app.env') == "local")
+                <h1 class="font-bold text-2xl sm:text-3xl dark:text-gray-200 text-gray-700">Run "<span class="text-blue-500">php artisan custom-tenant:create</span>" To create your first tenant </h1>
+            @else 
+                 <h1 class="font-bold text-2xl sm:text-3xl dark:text-gray-200 text-gray-700">Please Login to Create your First Application</h1>
+            @endif
+        @else
+            <h1 class="font-bold text-2xl sm:text-3xl dark:text-gray-200 text-gray-700">Add Your First Application</h1>
+        @endif
     @else
-        <h1 class="font-bold text-2xl sm:text-3xl  dark:text-gray-200 text-gray-700">Add Another Application</h1>
+        @if(Auth::user())
+            <h1 class="font-bold text-2xl sm:text-3xl  dark:text-gray-200 text-gray-700">Add Another Application</h1>
+        @endif
     @endif
 </div>
