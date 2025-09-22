0a641a547d9c9297ebd44bfdd7f4b1f2461a11f4
Neil Mallia <neil.mallia@9hdigital.com>
2025-09-22 05:40:04 +0200

    Turned the Login Audit Logs into a tool page

---
+++ nova-components/LoginLogs/src/LoginTrail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace IslandServices\LoginLogs;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Menu\MenuSection;
+use Laravel\Nova\Nova;
+use Laravel\Nova\Tool;
+
+class LoginTrail extends Tool
+{
+    /**
+     * Perform any tasks that need to happen when the tool is booted.
+     */
+    public function boot(): void
+    {
+        Nova::mix('login-logs', __DIR__ . '/../dist/mix-manifest.json');
+    }
+
+    /**
+     * Build the menu that renders the navigation links for the tool.
+     */
+    public function menu(Request $request): MenuSection
+    {
+        return MenuSection::make('Login')
+            ->path('/login-logs')
+            ->icon('clipboard-document-list');
+    }
+}
