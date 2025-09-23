7e9f2c3dafd6f4b7fcabd4f5ac60b43145e0c9f1
Neil Mallia <neil.mallia@9hdigital.com>
2025-09-23 16:18:41 +0200

    Added Lenses for Collection Notes

---
+++ app/Nova/Lenses/Post/CollectionNote/UnprocessedCollectionNote.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Nova\Lenses\Post\CollectionNote;
+
+use App\Nova\Parts\Post\SharedFields\OrderLensFields;
+use App\Traits\LensPolicy;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Contracts\Pagination\Paginator;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Card;
+use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Filters\Filter;
+use Laravel\Nova\Http\Requests\LensRequest;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Lenses\Lens;
+use Laravel\Nova\Query\Search\SearchableRelation;
+
+class UnprocessedCollectionNote extends Lens
+{
+    use LensPolicy;
+
+    public static string $policyKey = "unprocessed collection_note";
+
+    public static function searchableColumns(): array
+    {
+        return [
+            'collection_note_number',
+            new SearchableRelation('customer', 'client'),
+            new SearchableRelation('customer', 'account_number'),
+        ];
+    }
+
+    /**
+     * Get the query builder / paginator for the lens.
+     */
+    public static function query(LensRequest $request, Builder $query): Builder|Paginator
+    {
+        return $request->withOrdering($request->withFilters(
+            $query->where('status', 0)
+        ));
+    }
+
+    /**
+     * Get the fields available to the lens.
+     * @return array<int, Field>
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return (new OrderLensFields())('collection_note');
+    }
+
+    /**
+     * Get the cards available on the lens.
+     * @return array<int, Card>
+     */
+    public function cards(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the lens.
+     * @return array<int, Filter>
+     */
+    public function filters(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available on the lens.
+     * @return array<int, Action>
+     */
+    public function actions(NovaRequest $request): array
+    {
+        return parent::actions($request);
+    }
+
+    /**
+     * Get the URI key for the lens.
+     */
+    public function uriKey(): string
+    {
+        return 'unprocessed-collection-note';
+    }
+}
