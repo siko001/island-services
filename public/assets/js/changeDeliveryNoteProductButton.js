a97ce076a384809908006132f140ca5e8b174f23
Neil Mallia <neil.mallia@9hdigital.com>
2025-09-23 14:00:39 +0200

    Added the direct sale to the javascript file to change button text

--- public/assets/js/changeDeliveryNoteProductButton.js
+++ public/assets/js/changeDeliveryNoteProductButton.js
@@ -1,13 +1,19 @@
 // Modify the button to attach product to delivery not (original: create)
 document.addEventListener('inertia:navigate', (event) => {
-	if(window.location.pathname.startsWith('/admin/resources/delivery-notes')) {
+	if(window.location.pathname.startsWith('/admin/resources/delivery-notes') || window.location.pathname.startsWith('/admin/resources/direct-sales')) {
 		let attempts = 0;
 		const maxAttempts = 50; // stop after ~5 seconds
 
 		const resourceButtonChange = setInterval(() => {
 			attempts++;
 
-			const container = document.querySelector('[data-relationship="deliveryNoteProducts"]');
+			let container;
+			const deliveryNoteContainer = document.querySelector('[data-relationship="deliveryNoteProducts"]');
+			const directSaleContainer = document.querySelector('[data-relationship="directSaleProducts"]');
+
+			deliveryNoteContainer ? container = deliveryNoteContainer : null;
+			directSaleContainer ? container = directSaleContainer : null;
+
 			const buttonParent = container?.children[1]?.children[1]?.children[0]?.children[0];
 			const desktopText = buttonParent?.children[0];
 			const mobileText = buttonParent?.children[1];
